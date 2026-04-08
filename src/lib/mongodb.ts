259324e728a8579e0439ad1d05c002b1d5d5320d
Tefikujkani <149491750+Tefikujkani@users.noreply.github.com>
2026-04-08 19:10:17 +0200

    Prepare for deployment

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,51 +1,51 @@
-import mongoose from 'mongoose'
+// import mongoose from 'mongoose'
 
-if (!process.env.MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URI environment variable')
-}
+// if (!process.env.MONGODB_URI) {
+//   throw new Error('Please define the MONGODB_URI environment variable')
+// }
 
-const MONGODB_URI = process.env.MONGODB_URI
+// const MONGODB_URI = process.env.MONGODB_URI
 
-interface MongooseConnection {
-  conn: typeof mongoose | null;
-  promise: Promise<typeof mongoose> | null;
-}
+// interface MongooseConnection {
+//   conn: typeof mongoose | null;
+//   promise: Promise<typeof mongoose> | null;
+// }
 
-declare global {
-  var mongoose: MongooseConnection | undefined;
-}
+// declare global {
+//   var mongoose: MongooseConnection | undefined;
+// }
 
-let cached = global.mongoose;
+// let cached = global.mongoose;
 
-if (!cached) {
-  cached = global.mongoose = {
-    conn: null,
-    promise: null,
-  };
-}
+// if (!cached) {
+//   cached = global.mongoose = {
+//     conn: null,
+//     promise: null,
+//   };
+// }
 
-async function dbConnect(): Promise<typeof mongoose> {
-  if (cached?.conn) {
-    return cached.conn;
-  }
+// async function dbConnect(): Promise<typeof mongoose> {
+//   if (cached?.conn) {
+//     return cached.conn;
+//   }
 
-  if (!cached?.promise) {
-    const opts = {
-      bufferCommands: false,
-    };
+//   if (!cached?.promise) {
+//     const opts = {
+//       bufferCommands: false,
+//     };
 
-    cached!.promise = mongoose.connect(MONGODB_URI).then((mongoose) => {
-      return mongoose;
-    });
-  }
+//     cached!.promise = mongoose.connect(MONGODB_URI).then((mongoose) => {
+//       return mongoose;
+//     });
+//   }
 
-  try {
-    cached!.conn = await cached?.promise;
-    return cached!.conn;
-  } catch (e) {
-    cached!.promise = null;
-    throw e;
-  }
-}
+//   try {
+//     cached!.conn = await cached?.promise;
+//     return cached!.conn;
+//   } catch (e) {
+//     cached!.promise = null;
+//     throw e;
+//   }
+// }
 
-export default dbConnect 
+// export default dbConnect
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,31 +1,31 @@
-import mongoose from 'mongoose'
+// import mongoose from 'mongoose'
 
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  image: {
-    type: String,
-  },
-  role: {
-    type: String,
-    enum: ['user', 'admin'],
-    default: 'user',
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-})
+// const UserSchema = new mongoose.Schema({
+//   name: {
+//     type: String,
+//     required: true,
+//   },
+//   email: {
+//     type: String,
+//     required: true,
+//     unique: true,
+//   },
+//   password: {
+//     type: String,
+//     required: true,
+//   },
+//   image: {
+//     type: String,
+//   },
+//   role: {
+//     type: String,
+//     enum: ['user', 'admin'],
+//     default: 'user',
+//   },
+//   createdAt: {
+//     type: Date,
+//     default: Date.now,
+//   },
+// })
 
-export default mongoose.models.User || mongoose.model('User', UserSchema) 
+// export default mongoose.models.User || mongoose.model('User', UserSchema)
